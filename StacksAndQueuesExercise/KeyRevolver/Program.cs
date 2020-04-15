0fd84f12dd2ad6852566d3f37e3a119ccc4a0c6c
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-15 15:38:50 +0300

    create Key Revolver task

---
+++ StacksAndQueuesExercise/KeyRevolver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceOfBullet = int.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+            var bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int intelligence = int.Parse(Console.ReadLine());
+        }
+    }
+}
