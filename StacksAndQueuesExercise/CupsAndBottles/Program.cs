a2356edd5fc77d7deba254dccbfca73e5d494e17
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-15 16:51:12 +0300

    create new task Cups and Bottles

---
+++ StacksAndQueuesExercise/CupsAndBottles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CupsAndBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cupsCapacity = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            var bottleWater = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            var amount = 0;
+
+            while (true)
+            {
+                if (cupsCapacity.Count == 0 || bottleWater.Count == 0)
+                    break;
+
+                var cup = cupsCapacity.Dequeue();
+                var bottle = bottleWater.Pop();
+
+                amount += bottle - cup;
+            }
+
+            if(bottleWater.Count > 0)
+                Console.WriteLine($"Bottles: {bottleWater.Count}");
+            else if(cupsCapacity.Count > 0)
+                Console.WriteLine($"Cups: {cupsCapacity.Count}");
+
+            Console.WriteLine($"Wasted litters of water: {amount}");
+        }
+    }
+}
