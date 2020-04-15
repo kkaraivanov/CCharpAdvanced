73bf3871474e4a861cff4357678c6dda7e2afbe6
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-15 16:22:23 +0300

    task complete

--- StacksAndQueuesExercise/KeyRevolver/Program.cs
+++ StacksAndQueuesExercise/KeyRevolver/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace KeyRevolver
@@ -8,10 +9,52 @@ namespace KeyRevolver
         static void Main(string[] args)
         {
             int priceOfBullet = int.Parse(Console.ReadLine());
-            int gunBarrelSize = int.Parse(Console.ReadLine());
-            var bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            var locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int intelligence = int.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine()); // count bullet in barrel
+            var bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            var locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            var intelligence = int.Parse(Console.ReadLine());
+
+            int countBullet = bullets.Count;
+            int counterBullet = 0;
+            ;
+            while (true)
+            {
+                if(bullets.Count == 0 || locks.Count == 0)
+                    break;
+
+                var bullet = bullets.Peek();
+                var lok = locks.Peek();
+
+                if (bullet <= lok)
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                bullets.Pop();
+                counterBullet++;
+                if (counterBullet == gunBarrelSize && bullets.Count != 0)
+                {
+                    counterBullet = 0;
+                    Console.WriteLine("Reloading!");
+                }
+            }
+
+            countBullet -= bullets.Count;
+            var bulletsCost = countBullet * priceOfBullet;
+
+            if (locks.Count == 0)
+            {
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligence - bulletsCost}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
         }
     }
 }
