bc9a58e16e8ef58e361fba1783a69fbce9308a78
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-16 10:16:52 +0300

    task Primary Diagonal complete

--- MultidimensionalArraysLab/PrimaryDiagonal/Program.cs
+++ MultidimensionalArraysLab/PrimaryDiagonal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PrimaryDiagonal
 {
@@ -6,7 +7,36 @@ namespace PrimaryDiagonal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            var matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var row = InputArr();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+
+            int step = 0;
+            int result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                result += matrix[i, step];
+                step++;
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static int[] InputArr()
+        {
+            return Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
         }
     }
 }
