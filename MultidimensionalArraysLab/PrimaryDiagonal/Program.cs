24ed5646d21af7e8a654b0ed3e37f11c032d6d47
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-16 10:07:10 +0300

    create new task Primary Diagonal

---
+++ MultidimensionalArraysLab/PrimaryDiagonal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
