909bd1b662c3302abdb3f5e5bf70df3998fbd08e
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-27 12:40:41 +0300

    complete Add VAT

---
+++ FunctionalProgrammingLab/AddVAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, Result));
+        }
+
+        static string[] Result =>
+            Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => $"{x += x * 0.20:f2}")
+                .ToArray();
+    }
+}
