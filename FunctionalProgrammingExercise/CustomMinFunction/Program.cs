1ea5464d64ccfe6f437f370b69fe2ce2a8ade85b
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-27 16:11:31 +0300

    Knights of Honor complete

---
+++ FunctionalProgrammingExercise/CustomMinFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ FunctionalProgrammingExercise/KnightsOfHonor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Func<string, string> filter = x => $"Sir {x}";
+            Action<string> print = x 
+                => Console.WriteLine(string.Join(Environment.NewLine
+                    , x
+                        .Split()
+                        .Select(filter)
+                        .ToArray()));
+
+            print(input);
+        }
+    }
+}
