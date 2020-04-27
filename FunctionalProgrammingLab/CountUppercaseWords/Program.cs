b77bd8e76600c1391d2a45aa45bb7e1884562121
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-27 11:58:16 +0300

    complete Count Uppercase Words

---
+++ FunctionalProgrammingLab/CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(
+                Environment.NewLine
+                , Console
+                    .ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Where(x => char.IsUpper(x[0]))
+                    .ToArray()));
+        }
+    }
+}
