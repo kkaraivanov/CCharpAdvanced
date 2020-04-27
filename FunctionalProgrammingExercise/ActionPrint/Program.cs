ed03e451d726385123adae8281352e958ae75cb9
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-27 15:59:15 +0300

    Action Print  complete

---
+++ FunctionalProgrammingExercise/ActionPrint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Action<string> print = x 
+                => Console.WriteLine(string.Join(Environment.NewLine, x.Split().ToArray()));
+
+            print(input);
+        }
+    }
+}
