2a94dc27afff80f5ee0e455a88cdbf33dbb41cc2
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-04-29 14:39:13 +0300

    Custom Comparator complete

---
+++ FunctionalProgrammingExercise/CustomComparator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Func<int, int, int> comparator = (x, y) =>
+                x % 2 == 0 && y % 2 != 0 ? -1 :
+                y % 2 == 0 && x % 2 != 0 ? 1 : 
+                x.CompareTo(y);
+            Comparison<int> comparision = new Comparison<int>(comparator);
+            Array.Sort(arr, comparision);
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+    }
+}
