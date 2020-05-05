b659591c178b0386220fef6c660929be6a9675f6
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-05-05 22:04:45 +0300

    create new solution Generics Lab

---
+++ GenericsLab/Box/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Box
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
