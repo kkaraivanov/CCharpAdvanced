80e1a8695c9ccce6676d0c5e617fc1ce2371b45e
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-05-11 13:17:15 +0300

    complete Array Creator

---
+++ GenericsLab/GenericScale/EqualityScale.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericScale
+{
+    using System;
+
+    public class EqualityScale<T>
+        where T : IComparable<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public T AreEqual() => this.left.CompareTo(this.right) > 0 ? this.left : this.right;
+    }
+}
