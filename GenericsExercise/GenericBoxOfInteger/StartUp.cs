1afd6558eef4f9895e12624bfc1512e38f6c0896
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-05-11 14:27:00 +0300

    complete Box of string and int

---
+++ GenericsExercise/GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericBoxOfInteger
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var inputOfType = new BoxOfType<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                inputOfType.Value = int.Parse(Console.ReadLine());
+                Console.WriteLine(inputOfType);
+            }
+        }
+    }
+}
---
+++ GenericsExercise/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericBoxOfString
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var inputOfType = new BoxOfType<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                inputOfType.Value = Console.ReadLine();
+                Console.WriteLine(inputOfType);
+            }
+        }
+    }
+}
