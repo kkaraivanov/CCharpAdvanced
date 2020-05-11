79acd8aa8ba1ced7248b7f5f19998491c8040f64
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-05-11 15:39:20 +0300

    complete count methods

---
+++ GenericsExercise/GenericCountMethodDouble/GenericClass.cs
@@ -0,0 +1,39 @@
+﻿namespace GenericCountMethodDouble
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class GenericClass<T>
+        where T : IComparable<T>
+    {
+        private List<T> item;
+
+        public GenericClass()
+        {
+            this.item = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.item.Add(item);
+        }
+
+        public void Swap(int ferstIndex, int secondIndex)
+        {
+            var temp = this.item[ferstIndex];
+            this.item[ferstIndex] = this.item[secondIndex];
+            this.item[secondIndex] = temp;
+        }
+
+        public int Count(T item)
+        {
+            return this.item.Where(x => x.CompareTo(item) > 0).Count();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, this.item.Select(x => $"{typeof(T)}: {x}"));
+        }
+    }
+}
