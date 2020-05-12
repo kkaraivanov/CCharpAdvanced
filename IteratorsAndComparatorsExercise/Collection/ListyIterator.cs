12cc61530547c3a6246fe61b7e8e6d4d5ae6a24d
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-05-12 12:33:54 +0300

    task Collection not complete

---
+++ IteratorsAndComparatorsExercise/Collection/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿namespace Collection
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> items;
+        private int index;
+
+        public ListyIterator(params T[] items)
+        {
+            this.items = new List<T>(items);
+        }
+
+        public bool Move()
+        {
+            if (this.index < this.items.Count - 1)
+            {
+                this.index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext() => this.index + 1 < this.items.Count;
+
+        public void Print()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.items[this.index]);
+        }
+
+        public IEnumerator<T> GetEnumerator() => this.items.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ IteratorsAndComparatorsExercise/ListyIterator/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace ListyIterator
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> iterator = null;
+            string command = null;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var action = command.Split();
+
+                switch (action[0])
+                {
+                    case "Create":
+                        iterator = new ListyIterator<string>(action.Skip(1).ToArray());
+                        break;
+                    case "Move":
+                        Console.WriteLine(iterator.Move());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            iterator.Print();
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(iterator.HasNext());
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
