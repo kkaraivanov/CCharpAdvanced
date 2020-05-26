3a4b873f7a11272509ed9778cf4f7ddcd90475b2
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-05-26 17:00:40 +0300

    refactoring Lootbox methods and propertyes

--- Exams/Lootbox/Program.cs
+++ Exams/Lootbox/Program.cs
@@ -10,7 +10,19 @@
         private static Stack<int> secondBox;
         private static List<int> items;
 
-        static void Main(string[] args)
+        private static string DisplayLoot() => items.Sum() >= 100 ? $"Your loot was epic! Value: {items.Sum()}" : $"Your loot was poor... Value: {items.Sum()}";
+
+        private static string DisplayBox() => firstBox.Count == 0 ? "First lootbox is empty" : "Second lootbox is empty";
+
+        private static bool GenericIsEmpty => firstBox.Count > 0 && secondBox.Count > 0;
+
+        private static int[] ReadLine() => Console
+            .ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        static void Main()
         {
             firstBox = new Queue<int>(ReadLine());
             secondBox = new Stack<int>(ReadLine());
@@ -37,11 +49,6 @@
             Console.WriteLine(DisplayLoot());
         }
 
-        private static void MoveElsements()
-        {
-            firstBox.Enqueue(secondBox.Pop());
-        }
-
         private static void AddElements(int firstElement, int secondElement)
         {
             items.Add(firstElement + secondElement);
@@ -53,16 +60,9 @@
             secondBox.Pop();
         }
 
-        private static string DisplayLoot() => items.Sum() >= 100 ? $"Your loot was epic! Value: {items.Sum()}" : $"Your loot was poor... Value: {items.Sum()}";
-
-        private static string DisplayBox() => firstBox.Count == 0 ? "First lootbox is empty" : "Second lootbox is empty";
-
-        private static bool GenericIsEmpty => firstBox.Count > 0 && secondBox.Count > 0;
-
-        private static int[] ReadLine() => Console
-            .ReadLine()
-            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            .Select(int.Parse)
-            .ToArray();
+        private static void MoveElsements()
+        {
+            firstBox.Enqueue(secondBox.Pop());
+        }
     }
 }
