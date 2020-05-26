b4fef234b07dfe0b17f8d664ee3027c763f19035
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-05-26 16:46:34 +0300

    complete Lootbox

---
+++ BasicAlgorithmsExercise/Quicksort/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Quicksort
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private void QuickSort<T>(T[] arr, int lo, int hi) where T : IComparable
+        {
+            int i = lo;
+            int j = hi;
+            IComparable pivot = arr[lo];
+
+            while (i <= j)
+            {
+                for (; (arr[i].CompareTo(pivot) < 0) && (i.CompareTo(hi) < 0); i++) ;
+                for (; (pivot.CompareTo(arr[j]) < 0) && (j.CompareTo(lo) > 0); j--) ;
+
+                if (i <= j)
+                    Swap(ref arr[i++], ref arr[j--]);
+
+            }
+            if (lo < j) QuickSort<T>(arr, lo, j);
+            if (i < hi) QuickSort<T>(arr, i, hi);
+        }
+        static void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
---
+++ Exams/Lootbox/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Lootbox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        private static Queue<int> firstBox;
+        private static Stack<int> secondBox;
+        private static List<int> items;
+
+        static void Main(string[] args)
+        {
+            firstBox = new Queue<int>(ReadLine());
+            secondBox = new Stack<int>(ReadLine());
+            items = new List<int>();
+
+            while (GenericIsEmpty)
+            {
+                int firstElement = firstBox.Peek();
+                int secondElement = secondBox.Peek();
+                bool isEven = (firstElement + secondElement) % 2 == 0;
+
+                if (isEven)
+                {
+                    items.Add(firstElement + secondElement);
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    MoveElsements();
+                }
+            }
+
+            Console.WriteLine(DisplayBox());
+            Console.WriteLine(DisplayLoot());
+        }
+
+        private static void MoveElsements()
+        {
+            firstBox.Enqueue(secondBox.Pop());
+        }
+
+        private static string DisplayLoot() => items.Sum() >= 100 ? $"Your loot was epic! Value: {items.Sum()}" : $"Your loot was poor... Value: {items.Sum()}";
+
+        private static string DisplayBox() => firstBox.Count == 0 ? "First lootbox is empty" : "Second lootbox is empty";
+
+        private static bool GenericIsEmpty => firstBox.Count > 0 && secondBox.Count > 0;
+
+        private static int[] ReadLine() => Console
+            .ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+    }
+}
