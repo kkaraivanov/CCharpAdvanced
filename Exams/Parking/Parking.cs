d9fe67c12085db3f6490befade7ba5f5cd0bdee2
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-06-29 23:32:02 +0300

    up exam

---
+++ Exams/Parking/Parking.cs
@@ -0,0 +1,48 @@
+﻿namespace Parking
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection.PortableExecutable;
+    using System.Runtime.InteropServices.ComTypes;
+    using System.Text;
+
+    public class Parking
+    {
+        private List<Car> data;
+        public string Type { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public Parking(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            data = new List<Car>();
+        }
+
+        public void Add(Car car)
+        {
+            if(Capacity > Count)
+                data.Add(car);
+        }
+
+        public bool Remove(string manufacturer, string model) =>  data.Remove(GetCar(manufacturer, model));
+
+        public Car GetCar(string manufacturer, string model) =>
+            data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+
+        public Car GetLatestCar() => data.OrderByDescending(x => x.Year).FirstOrDefault();
+
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {Type}:");
+            sb.AppendLine($"{string.Join(Environment.NewLine, data)}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
