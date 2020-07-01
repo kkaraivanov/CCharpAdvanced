c4f7ccd2dc8174363da3f0b034b1e2ff315be09b
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-01 20:13:21 +0300

    upload Person

---
+++ OOP/InheritanceExercise/Person/Child.cs
@@ -0,0 +1,24 @@
+﻿namespace Person
+{
+    using System;
+
+    public class Child : Person
+    {
+        public Child(string name, int age) 
+            : base(name, age)
+        {
+            this.Age = age;
+        }
+
+        public override int Age
+        {
+            protected set
+            {
+                if (value > 15)
+                    throw new ArgumentException($"Child's can not be able to have an age more than 15.");
+
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ OOP/InheritanceExercise/Person/Person.cs
@@ -0,0 +1,45 @@
+﻿namespace Person
+{
+    using System;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if(value.Length < 3)
+                    throw new ArgumentException("Name's length should not be less than 3 symbols!");
+
+                name = value;
+            }
+        }
+
+        public virtual int Age
+        {
+            get => age;
+            protected set
+            {
+                if(value < 0)
+                    throw new ArgumentException($"The age of {Name} can not be able to have a negative value.");
+                    
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
