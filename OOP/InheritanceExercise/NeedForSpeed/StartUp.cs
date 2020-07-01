2bdcac06069d73a9aaba5a2c8350e711bb084557
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-01 21:46:40 +0300

    upload PlayersAndMonsters

---
+++ OOP/InheritanceExercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace NeedForSpeed
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
