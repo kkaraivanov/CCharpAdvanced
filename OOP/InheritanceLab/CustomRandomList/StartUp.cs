a17c2034e00ffdd96cae419952201cbe2a23103c
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-01 18:39:50 +0300

    complete

---
+++ OOP/InheritanceLab/CustomRandomList/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomRandomList
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] colection = new[] {"pesho", "gosho", "stoian"};
+            RandomList rl = new RandomList(colection);
+
+            Console.WriteLine(rl.RandomString());
+        }
+    }
+}
