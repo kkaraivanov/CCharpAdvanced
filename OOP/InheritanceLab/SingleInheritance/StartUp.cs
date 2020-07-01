e076bdba12a2d1e15a7b7d4de268fd3481d6863c
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-01 17:25:13 +0300

    new tasks inherit class

---
+++ OOP/InheritanceLab/SingleInheritance/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Farm
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+        }
+    }
+}
