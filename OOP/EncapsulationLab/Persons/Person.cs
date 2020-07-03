e58c226e3dcdf2de0b13f33396dd69c206d93db8
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-03 20:51:35 +0300

    upload Encapsulation - Lab code

---
+++ OOP/EncapsulationLab/Persons/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private readonly string name;
+        private readonly string lastName;
+        private int age;
+
+        public Person(string name, string lastName, int age)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public string FirstName => this.name;
+
+        public string LastName => this.lastName;
+
+        public int Age
+        {
+            get => this.age;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {lastName} is {age} years old.";
+        }
+    }
+}
---
+++ OOP/EncapsulationLab/Salary/Person.cs
@@ -0,0 +1,47 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private readonly string name;
+        private readonly string lastName;
+        private readonly int age;
+        private decimal salary;
+
+        public Person(string name, string lastName, int age, decimal salary)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+            Salary = salary;
+        }
+
+        public string FirstName => this.name;
+
+        public string LastName => this.lastName;
+
+        public int Age => this.age;
+
+        public decimal Salary
+        {
+            get => this.salary;
+            private set => this.salary = value;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
