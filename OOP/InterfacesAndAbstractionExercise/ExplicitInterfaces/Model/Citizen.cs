59bdd4f3ab34acf75a7ce8c776bde3701a803aab
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-12 17:19:44 +0300

    upload completed project Explicit Interfaces

---
+++ OOP/InterfacesAndAbstractionExercise/ExplicitInterfaces/Model/Citizen.cs
@@ -0,0 +1,32 @@
+﻿namespace ExplicitInterfaces.Model
+{
+    using System.Security;
+    using Interface;
+
+    public class Citizen : IPerson, IResident
+    {
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public int Age { get; set; }
+
+        public Citizen(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public string GetName()
+        {
+            return $"{Name}";
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {Name}";
+        }
+
+    }
+}
---
+++ OOP/InterfacesAndAbstractionExercise/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace ExplicitInterfaces
+{
+    using System;
+    using System.Linq;
+    using Interface;
+    using Model;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            string input = null;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var currentLine = input.Split().ToArray();
+                var citizen = new Citizen(currentLine[0], int.Parse(currentLine[2]), currentLine[1]);
+
+                Console.WriteLine(((IPerson)citizen).GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+            }
+        }
+    }
+}
