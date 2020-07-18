ec99ae7d80aaee3ffba0ea6304f2ac5cb84c310a
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-18 17:18:47 +0300

    upload completed project Stealer

---
+++ OOP/ReflectionAndAttributesLab/Stealer/Hacker.cs
@@ -0,0 +1,24 @@
+﻿namespace Stealer
+{
+    public class Hacker
+    {
+        public string username = "securityGod82";
+        private string password = "mySuperSecretPassw0rd";
+
+        private int Id { get; set; }
+
+        public string Password 
+        { 
+            get => password; 
+            set => password = value;
+        }
+
+        public double BankAccountBalance { get; private set; }
+
+        public void DownloadAllBankAccountsInTheWorld()
+        {
+
+        }
+
+    }
+}
---
+++ OOP/ReflectionAndAttributesLab/Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fields)
+        {
+            var classType = Type.GetType(className);
+            var classFields = classType
+                .GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            var sb = new StringBuilder();
+            Object obj = Activator.CreateInstance(classType, new object?[] { });
+            sb.AppendLine($"Class under investigation: {className}");
+
+            foreach (FieldInfo classField in classFields
+                .Where(x => fields.Contains(x.Name)))
+            {
+                sb.AppendLine($"{classField.Name} = {classField.GetValue(obj)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
