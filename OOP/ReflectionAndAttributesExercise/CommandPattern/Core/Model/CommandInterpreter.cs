6d9fd8176f8909aa5acc59e01209dc84d3b56e41
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-18 23:43:08 +0300

    upload skeleton for solution Reflection and Attributes Exercise

---
+++ OOP/ReflectionAndAttributesExercise/CommandPattern/Core/Model/CommandInterpreter.cs
@@ -0,0 +1,12 @@
+﻿namespace CommandPattern.Core.Model
+{
+    using Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ OOP/ReflectionAndAttributesExercise/CommandPattern/Core/Model/Engine.cs
@@ -0,0 +1,17 @@
+﻿namespace CommandPattern.Core.Model
+{
+    using Contracts;
+
+    public class Engine : IEngine
+    {
+        public Engine(ICommandInterpreter command)
+        {
+
+        }
+
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
