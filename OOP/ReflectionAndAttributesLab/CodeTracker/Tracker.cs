c20ce984b91e0e0080b0c4a62b2aef66ed362861
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-18 23:18:26 +0300

    completed solution Reflection and Attributes Lab

---
+++ OOP/ReflectionAndAttributesLab/CodeTracker/Tracker.cs
@@ -0,0 +1,38 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            //var type = Assembly.GetExecutingAssembly().GetTypes();
+            var methods = typeof(StartUp).GetMethods(
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static)
+                .Select(x => new
+                {
+                    typeName = x,
+                    typeAttributes = x.GetCustomAttributes<AuthorAttribute>()
+                })
+                .Where(x => x.typeAttributes.Any());
+            //var typeAttributes = type
+            //    .Select(x => new
+            //    {
+            //        typeName = x,
+            //        typeAttributes = x.GetCustomAttributes<AuthorAttribute>()
+            //    })
+            //    .Where(x => x.typeAttributes.Any());
+
+            foreach (var method in methods)
+            {
+                var typeName = method.typeName.Name;
+                var attributes = method.typeAttributes.Select(x => x.Name);
+                var attributeName = string.Join(", ", attributes);
+
+                Console.WriteLine($"{typeName} is written by {attributeName}");
+            }
+        }
+    }
+}
---
+++ OOP/ReflectionAndAttributesLab/CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthorProblem
+{
+    using System;
+
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
